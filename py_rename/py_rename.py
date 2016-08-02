b2c932fb6761b0742513060cae0d355eddbda9b1
nagracks <nagracks@users.noreply.github.com>
2016-08-02 09:42:26 +0530

    Changes in docstrings

--- py_rename/py_rename.py
+++ py_rename/py_rename.py
@@ -15,16 +15,19 @@ __contributors__    = ["kretusmaximus", "astonge", "prabhath6", "Luki138"]
 
 class RenameIt(object):
 
-    """RenameIt has various methods for rename files
-    eg:
+    """Class RenameIt
 
+    Constructor args:
+    filename
+    dryrun
+    silent
+
+    methods
     * prefix_it
     * postfix_it
     * lower_it
-    * remove_space
+    * replace_space
     * camel_case
-    * regex_replace
-
     """
 
     def __init__(self, filename, dryrun, silent):
@@ -42,8 +45,7 @@ class RenameIt(object):
     def _print(self, *msg):
         """Print msg if not silent
 
-        :param msg: What to print
-        :type msg: str
+        :msg: str, What to print
         """
         if not self.silent:
             print(msg)
@@ -51,19 +53,16 @@ class RenameIt(object):
     def _rename(self, new_name):
         """Generic rename method with error handling
 
-        :param old_name: File to rename
-        :type old_name: str
-        :param new_name: Filename to rename to
-        :type new_name: str
-        :return:
+        :new_name: str, Filename to rename to
+        :return: None
         """
-
         try:
             if not self.do_dryrun:
                 os.rename(self.full_name, new_name + self.extension)
             self._print("renaming: {old} --> {new}".format(old=self.full_name,
-                                                           new=new_name + self.extension))
-            # set after every rename, makes it possible to run multiple arguments
+                                                new=new_name + self.extension))
+            # Set after every rename, 
+            # Makes it possible to run multiple arguments
             self.filename = new_name
             self.full_name = self.filename + self.extension
         except OSError as e:
@@ -74,9 +73,8 @@ class RenameIt(object):
     def prefix_it(self, prefix_str):
         """Prefix filename with prefix string
 
-        :prefix_str: string, string to use as prefix in filename
+        :prefix_str: str, string to use as prefix in filename
         :returns: None
-
         """
         old_name = self.filename
         new_name = prefix_str + old_name
@@ -85,9 +83,8 @@ class RenameIt(object):
     def postfix_it(self, postfix_str):
         """Postfix filename with postfix string
 
-        :postfix_str: string, string to use as postfix in filename
+        :postfix_str: str, string to use as postfix in filename
         :returns: None
-
         """
         old_name = self.filename
         new_name = old_name + postfix_str
@@ -96,7 +93,6 @@ class RenameIt(object):
     def lower_it(self):
         """Lowercase the filename
         :returns: None
-
         """
         old_name = self.filename
         new_name = old_name.lower()
@@ -107,7 +103,6 @@ class RenameIt(object):
 
         :fill_char: default to '_'
         :returns: None
-
         """
         old_name = self.filename
         new_name = old_name.replace(' ', fill_char)
@@ -116,9 +111,7 @@ class RenameIt(object):
     def camel_case(self):
         """Convert to camel case
         :returns: None
-
         """
-
         old_name = self.filename.replace('_', ' ')
         modified_name = re.findall('[\w]+', old_name.lower())
         new_name = ''.join([word.title() for word in modified_name])
@@ -128,7 +121,6 @@ class RenameIt(object):
 def main():
     """Main function
     :returns: None
-
     """
 
     # Commandline args #
