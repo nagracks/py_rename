a83ac835403ed4c27ef5defd7bed8201351e03f3
nagracks <nagracks@users.noreply.github.com>
2017-02-27 20:17:46 +0530

    Delete cmd_args.py & tests/, Refactor

--- py_rename/py_rename.py
+++ py_rename/py_rename.py
@@ -3,7 +3,7 @@
 
 import os
 import re
-from cmd_args import parse_args
+from argparse import ArgumentParser
 
 
 __author__          = "nagracks"
@@ -131,12 +131,73 @@ class RenameIt(object):
 
 
 def main():
-    """Main function
-    :returns: None
-    """
+    parser = ArgumentParser(description="Python Rename")
+    parser.add_argument(
+            '-v',
+            '--version',
+            action='version',
+            version='%(prog)s version 0.1'
+            )
+    parser.add_argument(
+            '-A',
+            '--prefix',
+            dest='prefix',
+            metavar='string',
+            action='store',
+            help="prefix filename with prefix string"
+            )
+    parser.add_argument(
+            '-B',
+            '--postfix',
+            dest='postfix',
+            metavar='string',
+            action='store',
+            help="postfix filename with postfix string"
+            )
+    parser.add_argument(
+            '-r',
+            '--rename',
+            dest='rename',
+            metavar='string',
+            action='store',
+            help="replace filename with string"
+            )
+    parser.add_argument(
+            '-n',
+            '--dryrun',
+            dest='dryrun',
+            action='store_true',
+            help="perform a dry run (will not run any actions)"
+            )
+    parser.add_argument(
+            '--lower',
+            dest='lower',
+            action='store_true',
+            help="lowercase the filename"
+            )
+    parser.add_argument(
+            '--remove-space',
+            action='store_true',
+            help="remove space with underscore"
+            )
+    parser.add_argument(
+            '--camel-case',
+            action='store_true',
+            help="convert to camel case"
+            )
+    parser.add_argument(
+            '-s',
+            '--silent',
+            dest='silent',
+            action='store_true',
+            help="silence output"
+            )
+    parser.add_argument(
+            'filename',
+            help="filename"
+            )
 
-    # Commandline args #
-    args = parse_args()
+    args = parser.parse_args()
 
     # Initialise RenameIt object #
     rename_it = RenameIt(args.filename, args.dryrun, args.silent)
